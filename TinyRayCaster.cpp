7c5271e141fce20227cc4e205799bc7fa8559fd9
Gordon Zheng <gzheng6@students.cs.ubc.ca>
2026-02-20 20:05:38 -0800

    Added monster sprites

--- TinyRayCaster.cpp
+++ TinyRayCaster.cpp
@@ -1,6 +1,7 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <cstdint>
 #include <cassert>
@@ -10,13 +11,14 @@
 #include "map.h"
 #include "utils.h"
 #include "player.h"
+#include "sprite.h"
 #include "framebuffer.h"
 #include "textures.h"
 
-int wall_x_texcoord(const float x, const float y, Texture &tex_walls) {
-    float hitx = x - floor(x + .5); 
-    float hity = y - floor(y + .5); 
-    float edge = std::abs(hity) > std::abs(hitx) ? hity : hitx; // we need to determine whether we hit a vertical or horizontal wall
+int wall_x_texcoord(const float hitx, const float hity, Texture &tex_walls) {
+    float x = hitx - floor(hitx + .5); 
+    float y = hity - floor(hity + .5); 
+    float edge = std::abs(y) > std::abs(x) ? y : x; // we need to determine whether we hit a vertical or horizontal wall
     int tex = (edge + .5) * tex_walls.size;
     if (tex < 0) tex = 0; // prevent rounding errors from triggering the assert
     if (tex >= (int)tex_walls.size) tex = tex_walls.size - 1;
@@ -24,7 +26,38 @@ int wall_x_texcoord(const float x, const float y, Texture &tex_walls) {
     return tex;
 }
 
-void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls) {
+void map_show_sprite(Sprite &sprite, FrameBuffer &fb, Map &map) {
+	const size_t rect_w = fb.w/(map.w*2); // size of one map cell on screen
+	const size_t rect_h = fb.h/map.h;
+	fb.draw_rectangle(sprite.x*rect_w-3, sprite.y*rect_h-3, 6, 6, pack_color(255, 0, 0));
+}
+
+void draw_sprite(Sprite &sprite, std::vector<float> &depth_buffer, FrameBuffer &fb, Player &player, Texture &tex_sprites) {
+	// absolute direction from the player to the sprite (in radians)
+	float sprite_dir = atan2(sprite.y - player.y, sprite.x - player.x);
+	while (sprite_dir - player.a > M_PI) sprite_dir -= 2*M_PI; // remove unnecessary periods from the relative direction
+	while (sprite_dir - player.a < -M_PI) sprite_dir += 2*M_PI;
+
+	size_t sprite_screen_size = std::min(1000, static_cast<int>(fb.h/sprite.player_dist)); // screen sprite size
+	int h_offset = (sprite_dir-player.a)/player.fov*(fb.w/2)+(fb.w/2)/2-tex_sprites.size/2; //3d view takes half of fb
+	int v_offset = fb.h/2-sprite_screen_size/2;
+
+	for (size_t i=0; i<sprite_screen_size; i++) {
+		if (h_offset+int(i)<0 || h_offset+i>=fb.w/2) continue;
+		if (depth_buffer[h_offset+i]<sprite.player_dist) continue; // this sprite column is occluded
+		for (size_t j=0; j<sprite_screen_size; j++) {
+			if (v_offset+int(j)<0 || v_offset+j>=fb.h) continue;
+			uint32_t color = tex_sprites.get(i*tex_sprites.size/sprite_screen_size, j*tex_sprites.size/sprite_screen_size, sprite.tex_id);
+			uint8_t r, g, b, a;
+			unpack_color(color, r, g ,b, a);
+			if (a>128)
+				fb.set_pixel(fb.w/2+h_offset+i, v_offset+j, color);
+		}
+	}
+}
+
+
+void render(FrameBuffer &fb, Map &map, Player &player, std::vector<Sprite> &sprites, Texture &tex_walls, Texture &tex_monst) {
 	fb.clear(pack_color(255, 255, 255)); // clear screen
 
 	const size_t rect_w = fb.w/(map.w*2); // size of one map cell on the screen
@@ -40,6 +73,7 @@ void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls) {
 		}
 	}
 
+	std::vector<float> depth_buffer(fb.w/2, 1e3);
 	for (size_t i=0; i<fb.w/2; i++) { // draw visibility cone and the 3D view
 		float angle = player.a-player.fov/2 + player.fov*i/float(fb.w/2);
 		for (float t=0; t<20; t+=.01) { // ray marching loop
@@ -51,7 +85,9 @@ void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls) {
 
 			size_t texid = map.get(x,y) - '0'; // our ray touches a wall, so draw the vertical column (3D)
 			assert(texid<tex_walls.count);
-			size_t column_height = fb.h/(t*cos(angle-player.a));
+			float dist = t*cos(angle-player.a);
+			depth_buffer[i] = dist;
+			size_t column_height = fb.h/dist;
 			int x_texcoord = wall_x_texcoord(x, y, tex_walls);
 			std::vector<uint32_t> column = tex_walls.get_scaled_column(texid, x_texcoord, column_height);
 			int pix_x = i + fb.w/2; // right half of screen hence the +fb.w/2
@@ -64,33 +100,40 @@ void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls) {
 			break;
 		} // ray marching loop
 	} // field of viecw ray sweeping
+
+	for (size_t i=0; i<sprites.size(); i++) { // update the distances from the player to each sprite
+		sprites[i].player_dist = std::sqrt(pow(player.x - sprites[i].x, 2) + pow(player.y - sprites[i].y, 2));
+		std::sort(sprites.begin(), sprites.end()); // sort it from furthest to closest
+		for (size_t i=0; i<sprites.size(); i++) { // draw the sprites
+			map_show_sprite(sprites[i], fb, map);
+			draw_sprite(sprites[i], depth_buffer, fb, player, tex_monst);
+		}
+	}
 }
 
 int main() {
-	FrameBuffer fb; 
-	fb.w = 1024;
-	fb.h = 512; 
-	fb.img = std::vector<uint32_t>(1024*512, pack_color(255, 255, 255));
-    Player player;
-	player.x = 3.456;
-	player.y = 2.345; 
-	player.a = 1.523;
-	player.fov = M_PI/3.;
+	FrameBuffer fb{1024, 512, std::vector<uint32_t>(1024*512, pack_color(255, 255, 255))};
+    Player player{3.456, 2.345, 1.523, M_PI/3.};
     Map map;
     Texture tex_walls("texturesfolder/walltext.png");
-    if (!tex_walls.count) {
-		std::cerr << "Failed to load wall textures" << std::endl;
+	Texture tex_monst("texturesfolder/monsters.png");
+	if (!tex_walls.count || !tex_monst.count) {
+		std::cerr << "Failed to load textures" << std::endl;
 		return -1;
 	}
+	std::vector<Sprite> sprites{ {3.523, 3.812, 2, 0}, {1.834, 8.765, 0, 0}, {5.323, 5.365, 1, 0}, {4.123, 10.265, 1, 0} };
 
-	for (size_t frame=0; frame<360; frame++) {
-		std::stringstream ss;
-		ss << "out/" << std::setfill('0') << std::setw(5) << frame << ".ppm";
-		player.a += 2*M_PI/360;
+	// for (size_t frame=0; frame<360; frame++) {
+	// 	std::stringstream ss;
+	// 	ss << "out/" << std::setfill('0') << std::setw(5) << frame << ".ppm";
+	// 	player.a += 2*M_PI/360;
 		
-		render(fb, map, player, tex_walls);
-		drop_ppm_image(ss.str(), fb.img, fb.w, fb.h);
-	}
+	// 	render(fb, map, player, tex_walls);
+	// 	drop_ppm_image(ss.str(), fb.img, fb.w, fb.h);
+	// }
+
+	render(fb, map, player, sprites, tex_walls, tex_monst);
+	drop_ppm_image("./out.ppm", fb.img, fb.w, fb.h);
 
     return 0;
 }
